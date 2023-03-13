6567ff96997278ac6bc1be88e6c21686ec4ae113
Kev <kev162020@gmail.com>
2023-03-12 23:22:55 -0600

    Prueba 1: Envío datos a la app

--- functions/ConexionServidor.py
+++ functions/ConexionServidor.py
@@ -1,5 +1,7 @@
 #Librería para la conexión entre la raspberry y la app 
 
+#Esta función recibe como parametros la información a enviar y 
+#retorna un diccionario con los campos a llenar en la app movil
 def data_a_enviar(d1,d2,d3,d4,d5,d6,d7,d8):
     data = {
         'medicion1': d1,
@@ -10,5 +12,13 @@ def data_a_enviar(d1,d2,d3,d4,d5,d6,d7,d8):
         'medicion6': d6,
         'medicion7': d7,
         'medicion8': d8,
+    }
+    return data
+
+#Esta función solo envía un dato para el primer parametro de la app
+#movil
+def prueba_un_dato(dato):
+    data = {
+        'medicion1': dato,
     }
     return data
