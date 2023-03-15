bab55a2be0f30b6065c466617db93f224236f6da
kevcangas <kev162020@gmail.com>
2023-03-15 14:01:14 -0600

    Creación librerpua audio/Verificar DHT11

---
+++ functions/audio.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+#Reproduce el audio que se encuentre en la ruta
+def reproducir_audio(ruta_audio='/home/gato/TT2/programa_gato/data/audios/gato_1.mp3'):
+    playsound(ruta_audio)
+    return
