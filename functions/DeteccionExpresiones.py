160e4e983871ce5bed3d5a1dcaeebc05330e9339
Kev <kev162020@gmail.com>
2023-03-22 10:43:56 -0600

    Prueba librería DeteccionExpresion

--- functions/DeteccionExpresiones.py
+++ functions/DeteccionExpresiones.py
@@ -6,13 +6,13 @@ import numpy as np
 #Carga de la red neuronal convolucional entrenada
 #retorna el objeto ya instanciado
 def cargar_CNN():
-    model=tf.keras.models.load_model(r'data/redneuronal/DRostrosCNN.h5')
+    model=tf.keras.models.load_model(r'.\data\redneuronal\DRostrosCNN.h5')
     return model
 
 
 #Carga del clasificador Cascade para detección de rostros
 def cargar_Cascade():
-    face_cascade_name = 'data/redneuronal/haarcascade_frontalface_alt.xml'
+    face_cascade_name = r'.\data\redneuronal\haarcascade_frontalface_alt.xml'
     face_cascade = cv2.CascadeClassifier()
     if not face_cascade.load(face_cascade_name):
         print('Error al cargar Face Cascade')
@@ -33,17 +33,18 @@ def detectar_rostro(frame,face_cascade):
 
 #Función para la detección de las expresiones, retorna una etiqueta con el 
 #valor de la expresión detectada
-def deteccion_expresiones(cap, detector_rostros, red_convolucional, verbose:bool = False):
+def deteccion_expresiones(cap, detector_rostros, red_convolucional, verbose:bool = False, mostrar_ima:bool = False):
     #Procesamiento de la imagen
     ret, frame = cap.read() #Obtención de la imagen de la cámara
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Conversión a escala de grises
     frame = detectar_rostro(frame, detector_rostros) #Detección del rostro
+    frame_a_mostrar = frame
     frame = cv2.resize(frame, (48,48), interpolation = cv2.INTER_CUBIC) #Rescalamiento a 48x48 pixeles
 
     #Conversión de las imagenes de matriz a vector
     x_test = np.array(frame)
     x_test = np.expand_dims(x_test,axis=0)
-    prediccion = np.argmax(red_convolucional.predict(x_test, verbose=0)[0][0])
+    prediccion = np.argmax(red_convolucional.predict(x_test, verbose=0)[0])
 
     #Clasificación
     etiqueta = ''
@@ -62,4 +63,9 @@ def deteccion_expresiones(cap, detector_rostros, red_convolucional, verbose:bool
 
     if verbose: print(etiqueta)
     
+    if mostrar_ima: cv2.imshow('imagen',frame_a_mostrar)
+    
+    if cv2.waitKey(1) == ord('q'):
+        raise ValueError("Programa Detenido")
+
     return etiqueta
---
+++ tests/prueba_deteccion_expresiones_pc.py
@@ -0,0 +1,29 @@
+#Librerías externas
+import cv2
+
+#Librerías locales
+import functions.DeteccionExpresiones as DE
+
+
+#Inicialización
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+detector_rostros = DE.cargar_Cascade()
+red_convolucional = DE.cargar_CNN()
+
+
+#Programa principal
+try:
+    while(1):
+        DE.deteccion_expresiones(cap,
+                                 detector_rostros, 
+                                 red_convolucional, 
+                                 verbose=True, 
+                                 mostrar_ima=False)
+
+
+except ValueError as ve:
+    print(ve)
+
+
+except KeyboardInterrupt:
+    print('Programa detenido')
