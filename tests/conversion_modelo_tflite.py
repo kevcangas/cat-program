6568137e8799efc5c815525153a4746828647ec2
Kev <kev162020@gmail.com>
2023-04-05 15:40:55 -0600

    Conversion modelo tf a tflite

---
+++ tests/conversion_modelo_tflite.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+# Convert the model
+model=tf.keras.models.load_model(r'.\data\redneuronal\DRostrosCNN.h5')
+converter = tf.lite.TFLiteConverter.from_keras_model(model) # path to the SavedModel directory
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
