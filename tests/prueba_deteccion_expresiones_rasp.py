bd81c064e15539cfa542a0e1f5bd7b5ded721db3
kevcangas <kev162020@gmail.com>
2023-04-05 19:07:05 -0600

    Prueba expresiones rasp completada

---
+++ tests/prueba_deteccion_expresiones_rasp.py
@@ -0,0 +1,31 @@
+#PRUEBA: PRUEBA DE LA LIBRERÍA DETECCIONEXPRESIONES
+
+#Librerías externas
+import cv2
+
+#Librerías locales
+import functions.DeteccionExpresionesRasp as DE
+
+
+#Inicialización
+cap = cv2.VideoCapture(0)
+detector_rostros = DE.cargar_Cascade()
+red_convolucional = DE.cargar_CNN()
+
+
+#Programa principal
+try:
+    while(1):
+        DE.deteccion_expresiones(cap,
+                                 detector_rostros, 
+                                 red_convolucional, 
+                                 verbose=True, 
+                                 mostrar_ima=True)
+
+
+except ValueError as ve:
+    print(ve)
+
+
+except KeyboardInterrupt:
+    print('Programa detenido')
