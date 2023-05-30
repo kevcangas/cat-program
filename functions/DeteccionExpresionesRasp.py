1d7ad6db71ec1da8cb3dc25ececd1d2ae13585bf
kevcangas <kev162020@gmail.com>
2023-05-30 14:57:08 -0600

    Corrección red neuronal sin rostro

--- functions/DeteccionExpresionesRasp.py
+++ functions/DeteccionExpresionesRasp.py
@@ -28,9 +28,9 @@ def detectar_rostro(frame,face_cascade):
     faces = face_cascade.detectMultiScale(frame)
     if len(faces)!=0:
         (x,y,w,h)=faces[0]
-        return frame[y:y+h,x:x+w]
+        return frame[y:y+h,x:x+w],1
     else:
-        return frame
+        return frame,0
     
 
 #Función para la detección de las expresiones, retorna una etiqueta con el 
@@ -39,27 +39,31 @@ def deteccion_expresiones(cap, detector_rostros, red_convolucional, verbose:bool
     #Procesamiento de la imagen
     ret, frame = cap.read() #Obtención de la imagen de la cámara
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Conversión a escala de grises
-    frame = detectar_rostro(frame, detector_rostros) #Detección del rostro
-    frame_a_mostrar = frame
-    frame = cv2.resize(frame, (48,48), interpolation = cv2.INTER_CUBIC) #Rescalamiento a 48x48 pixeles
-
-    #Conversión de las imagenes de matriz a vector
-    x_test = np.array(frame)
-    x_test = np.expand_dims(x_test,axis=0)
-    x_test = np.expand_dims(x_test,axis=-1)
-    red_convolucional.set_tensor(0, x_test.astype(float32))
-    red_convolucional.invoke()
-    
-    predicciones = red_convolucional.get_tensor(32)[0]
-
-    #Clasificación
+    frame,flag = detectar_rostro(frame, detector_rostros) #Detección del rostro
     
     etiqueta = ''
 
-    if np.sort(predicciones)[-1] < 0.6:
-        etiqueta='No rostro' 
-        prediccion = 4
-    else:
+    if flag != 0:
+        frame_a_mostrar = frame
+        frame = cv2.resize(frame, (48,48), interpolation = cv2.INTER_CUBIC) #Rescalamiento a 48x48 pixeles
+
+        #Conversión de las imagenes de matriz a vector
+        x_test = np.array(frame)
+        x_test = np.expand_dims(x_test,axis=0)
+        x_test = np.expand_dims(x_test,axis=-1)
+        red_convolucional.set_tensor(0, x_test.astype(float32))
+        red_convolucional.invoke()
+        # output = red_convolucional.get_output_details()[0]
+        # print(output)
+        predicciones = red_convolucional.get_tensor(32)[0]
+
+        #print(predicciones)
+
+        #Clasificación
+        
+        
+
+        
         prediccion = np.argmax(predicciones)
 
         if prediccion == 0:  
@@ -74,8 +78,13 @@ def deteccion_expresiones(cap, detector_rostros, red_convolucional, verbose:bool
         if prediccion == 3: 
             etiqueta='Tristeza'
 
+    else:
+        etiqueta='No rostro' 
+        prediccion = 4
+        predicciones = [1,0]
+
     if verbose: print(f"Etiqueta: {etiqueta} Porcentaje: {np.sort(predicciones)[-1]}")
-    
+        
     if mostrar_ima: cv2.imshow('imagen',frame_a_mostrar)
     
     if cv2.waitKey(1) == ord('q'):
